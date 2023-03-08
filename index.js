afea735f4357ac44ebd6318ece290b27a0e6fdff
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-03-09 00:21:04 +0530

    connected with mongoDB database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.listen(5000,()=>{
 mongoose.connect('mongodb+srv://Mobiotics123:Mobiotics123@cluster0.yui8xl9.mongodb.net/?retryWrites=true&w=majority');
 console.log('Server is running on 5000')
     }catch(err){
-        console.log(err)
+        console.log(err);
     }
 })
 
