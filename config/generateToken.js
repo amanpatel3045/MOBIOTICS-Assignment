ccdc643c7610e34f576f0dc36a5d295cbc0c0c37
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-03-09 15:43:07 +0530

    Delete config directory

--- config/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id: id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIARY_TIME,
-  });
-};
-
-module.exports = generateToken;
